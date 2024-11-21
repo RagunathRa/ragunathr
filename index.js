bfce3ed2e005563dac2093fed8954f167a332384
ragunathr <ragunathr@tenacitics.in>
2024-11-21 21:47:04 +0530

    Fix ESLint issues and serve locally

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+/* eslint-disable no-unused-vars */
 function sayHello() {
-    var x = 6;
-
-    console.log("Hello, World!" , x);
-
-  }
+  const x = 6;
+  console.log("Hello, World!", x);
+  alert("Hello Everyone..! ")
+}
+/* eslint-enable no-unused-vars */
